2438c89bf23380bd389e42936e0d90f6ce01b370
Bruna Carvalho <68197761+bruna93@users.noreply.github.com>
2021-03-19 12:33:40 -0300

    :tada: new commit

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,42 @@
-export function Content() {
-  // Complete aqui
+import '../styles/content.scss';
+import { MovieCard } from './MovieCard';
+
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  GenreId: string;
+  movies: MovieProps[];
+}
+
+export function Content ({movies, GenreId}:ContentProps) {
+  return(
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {GenreId}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard
+              key={movie.Title}
+              title={movie.Title} 
+              poster={movie.Poster} 
+              runtime={movie.Runtime} 
+              rating={movie.Ratings[0].Value} 
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
