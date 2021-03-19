e97e53b21305466b1b96eaf400dcf8ef1cb868f9
Bruna Carvalho <68197761+bruna93@users.noreply.github.com>
2021-03-19 12:34:16 -0300

    :tada: new commit

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  selectedGenreId: number;
+  genres: GenreResponseProps[];
+  setGenreId: Function;
+}
+
+export function SideBar({selectedGenreId, genres, setGenreId}: SideBarProps) {
+  return (
+
+  <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => setGenreId(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+  </nav>
+  )
 }
